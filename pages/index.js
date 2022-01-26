d9e0530c18426d61dc17d181026dc5feceee1bd9
Thorarinng <42962891+Thorarinng@users.noreply.github.com>
2022-01-26 09:46:03 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-// retry
+// retry -
 
 export default function Home() {
   return (
